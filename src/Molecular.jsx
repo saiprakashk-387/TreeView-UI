658a23d618b96e519e1fbab3c9f72cb0bdfc0b1d
saiprakashk-387 <prakash387@yahoo.com>
2022-01-30 12:55:22 +0530

    Tree View

--- src/Molecular.jsx
+++ src/Molecular.jsx
@@ -10,7 +10,7 @@ export const Molecular = () => {
     const [FamilyMembers, setFamilyMembers] = useState(false)
     const [FamilyOne, setFamilyOne] = useState(false)
     const [FamilyTwo, setFamilyTwo] = useState(false)
- 
+
     useEffect(() => {
         getData();
     }, []);
@@ -24,19 +24,22 @@ export const Molecular = () => {
                 setChildFirst(res.data.termsrelation[0]?.terms)
                 setChildTwo(res.data.termsrelation[1]?.terms)
             })
-    } 
-    let plus=<i class="far fa-plus-square" style={{"color":"gray","backgroundColor":"whitesmoke","margin":"5px"}}></i>
-   let folder=<i class="fas fa-folder" style={{"color":"wheat", "margin":"5px"}}></i>
-   return <div style={{ "marginLeft": "650px" }}>
+    }
+    let plus = <i class="far fa-plus-square" style={{ "color": "gray", "backgroundColor": "whitesmoke", "margin": "5px" }}></i>
+    let folder = <i class="fas fa-folder" style={{ "color": "wheat", "margin": "5px" }}></i>
+
+    return <div style={{ "marginLeft": "650px" }}>
+
         <div class="form-group col-lg-4 col-md-6">
             <div class="card-header-action">
                 <li class="btn btn-primary" onClick={() => setFamilyMembers(!FamilyMembers)}
                 >   {plus}{folder}{res?.formated_name}</li>
             </div>
+
             {FamilyMembers ?
                 <>
-               <li onClick={() => setFamilyOne(!FamilyOne)} > &nbsp;&nbsp;&nbsp;&nbsp;  {plus} {folder} {data[0]?.rel} </li>
-                {FamilyOne ?
+                    <li onClick={() => setFamilyOne(!FamilyOne)} > &nbsp;&nbsp;&nbsp;&nbsp;  {plus} {folder} {data[0]?.rel} </li>
+                    {FamilyOne ?
                         <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{plus} {folder}{childFirst[0]?.name} </li>
                         : null}
                     <li onClick={() => setFamilyTwo(!FamilyTwo)}> &nbsp;&nbsp;&nbsp;&nbsp;   {plus}{folder}{data[1]?.rel}</li>
